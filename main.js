00fb398652ef1007685f5a3df0453b45c3f5807c
luis-rios <luis.rios.9815@gmail.com>
2022-03-20 23:09:21 -0600

    Primer commit: agregando estructura básica de html, css y un script generador de colores.

---
+++ main.js
@@ -0,0 +1,41 @@
+const colorPalette = {
+    colors: [],
+    methods: {
+        createRandomColors: (() => {
+            for (let index = 0; index < 4; index++) {
+                let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+                colorPalette.colors[index] = randomColor;
+            }
+        }),
+        render: (() => {
+            let html = '';
+            colorPalette.colors.forEach((color, index) => {
+                html += `
+                <div id="color-container${index+1}" style="background-color:#${color}" class="color-container">
+                    <div class="color-info">
+                        <p id="color-name${index+1}">#${color}</p>
+                    </div>
+                </div>
+                `
+            });
+            return html;
+        }) 
+    }
+}
+
+colorPalette.methods.createRandomColors();
+// console.log(colorPalette.methods.render());
+
+const generatorContainer = document.getElementById("generator");
+generatorContainer.innerHTML = colorPalette.methods.render();
+
+
+const botonContainer = document.getElementById('btn-row');
+
+botonContainer.addEventListener('click', (evento) => {
+    // console.log(evento.target.id);
+    if (evento.target.id === 'generate'){
+        colorPalette.methods.createRandomColors();
+        generatorContainer.innerHTML = colorPalette.methods.render();
+    }
+});
